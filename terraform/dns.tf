523b7aca3dfe80e5a3c3b656d3a55953e4ba2813
Maxwell Morris <github@maxwellmorris.me>
2022-12-17 20:15:37 +0000

    Added SPF record for mail service

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,3 +1,4 @@
+# Website records
 resource "cloudflare_record" "root_record" {
   zone_id    = var.cloudflare_zone_id
   name       = var.domain
@@ -15,6 +16,39 @@ resource "cloudflare_record" "www_record" {
   depends_on = [digitalocean_app.site_app]
 }
 
+# Mail service records
+resource "cloudflare_record" "mail_verify" {
+  zone_id    = var.cloudflare_zone_id
+  name       = var.domain
+  value      = "protonmail-verification=87a1d2b07ad3698186abeb79feeceb9f19833a81"
+  type       = "TXT"
+  ttl        = 3600
+}
+resource "cloudflare_record" "mail_mx_1" {
+  zone_id    = var.cloudflare_zone_id
+  name       = var.domain
+  value      = "mail.protonmail.ch"
+  type       = "MX"
+  priority   = "10"
+  ttl        = 3600
+}
+resource "cloudflare_record" "mail_mx_2" {
+  zone_id    = var.cloudflare_zone_id
+  name       = var.domain
+  value      = "mailsec.protonmail.ch"
+  type       = "MX"
+  priority   = "20"
+  ttl        = 3600
+}
+resource "cloudflare_record" "mail_spf" {
+  zone_id    = var.cloudflare_zone_id
+  name       = var.domain
+  value      = "v=spf1 include:_spf.protonmail.ch mx ~all"
+  type       = "TXT"
+  ttl        = 3600
+}
+
+# Website records (old domain)
 resource "cloudflare_record" "root_record_old" {
   zone_id    = var.cloudflare_zone_id_old
   name       = var.domain_old
