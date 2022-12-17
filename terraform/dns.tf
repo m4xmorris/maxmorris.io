ef9a11df3c2f4fa8ab55fe8be24b8f12275a2b8b
Maxwell Morris <github@maxwellmorris.me>
2022-12-17 20:12:55 +0000

    Testing

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,30 +15,6 @@ resource "cloudflare_record" "www_record" {
   depends_on = [digitalocean_app.site_app]
 }
 
-resource "cloudflare_record" "mail_verify_record" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = "protonmail-verification=87a1d2b07ad3698186abeb79feeceb9f19833a81"
-  type       = "TXT"
-  ttl        = 3600
-}
-resource "cloudflare_record" "mail_record_1" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = "mail.protonmail.ch"
-  type       = "MX"
-  priority   = "10"
-  ttl        = 3600
-}
-resource "cloudflare_record" "mail_record_2" {
-  zone_id    = var.cloudflare_zone_id
-  name       = var.domain
-  value      = "mailsec.protonmail.ch"
-  type       = "MX"
-  priority   = "20"
-  ttl        = 3600
-}
-
 resource "cloudflare_record" "root_record_old" {
   zone_id    = var.cloudflare_zone_id_old
   name       = var.domain_old
