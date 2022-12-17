a55ab007f8f45cbe762312f5de3293c84233eaf9
Maxwell Morris <github@maxwellmorris.me>
2022-12-17 22:34:48 +0000

    Switched to "auto" DNS TTL

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,7 +4,6 @@ resource "cloudflare_record" "root_record" {
   name       = "@"
   value      = replace(digitalocean_app.site_app.default_ingress, "/(https://)|(/)/", "")
   type       = "CNAME"
-  ttl        = 3600
   depends_on = [digitalocean_app.site_app]
 }
 resource "cloudflare_record" "www_record" {
@@ -12,7 +11,6 @@ resource "cloudflare_record" "www_record" {
   name       = "www"
   value      = replace(digitalocean_app.site_app.default_ingress, "/(https://)|(/)/", "")
   type       = "CNAME"
-  ttl        = 3600
   depends_on = [digitalocean_app.site_app]
 }
 
@@ -22,7 +20,6 @@ resource "cloudflare_record" "mail_verify" {
   name       = "@"
   value      = "protonmail-verification=87a1d2b07ad3698186abeb79feeceb9f19833a81"
   type       = "TXT"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_mx_1" {
   zone_id    = var.cloudflare_zone_id
@@ -30,7 +27,6 @@ resource "cloudflare_record" "mail_mx_1" {
   value      = "mail.protonmail.ch"
   type       = "MX"
   priority   = "10"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_mx_2" {
   zone_id    = var.cloudflare_zone_id
@@ -38,40 +34,34 @@ resource "cloudflare_record" "mail_mx_2" {
   value      = "mailsec.protonmail.ch"
   type       = "MX"
   priority   = "20"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_spf" {
   zone_id    = var.cloudflare_zone_id
   name       = "@"
   value      = "v=spf1 include:_spf.protonmail.ch mx ~all"
   type       = "TXT"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_dkim_1" {
   zone_id    = var.cloudflare_zone_id
   name       = "protonmail._domainkey"
   value      = "protonmail.domainkey.dbjm65arcatqxgwv5qlkmggadpqlpbr6lbnpz6y64qtpfm474d3fq.domains.proton.ch."
   type       = "CNAME"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_dkim_2" {
   zone_id    = var.cloudflare_zone_id
   name       = "protonmail2._domainkey"
   value      = "protonmail2.domainkey.dbjm65arcatqxgwv5qlkmggadpqlpbr6lbnpz6y64qtpfm474d3fq.domains.proton.ch."
   type       = "CNAME"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_dkim_3" {
   zone_id    = var.cloudflare_zone_id
   name       = "protonmail3._domainkey"
   value      = "protonmail3.domainkey.dbjm65arcatqxgwv5qlkmggadpqlpbr6lbnpz6y64qtpfm474d3fq.domains.proton.ch."
   type       = "CNAME"
-  ttl        = 3600
 }
 resource "cloudflare_record" "mail_dmarc" {
   zone_id    = var.cloudflare_zone_id
   name       = "_dmarc"
   value      = "v=DMARC1; p=quarantine"
   type       = "TXT"
-  ttl        = 3600
 }
