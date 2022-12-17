93e3d2ad83399df3bd339728f4f1c42af2509b91
Maxwell Morris <github@maxwellmorris.me>
2022-12-17 20:07:27 +0000

    Added verification DNS record for mail service

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,6 +15,14 @@ resource "cloudflare_record" "www_record" {
   depends_on = [digitalocean_app.site_app]
 }
 
+resource "cloudflare_record" "mail_verify_record" {
+  zone_id    = var.cloudflare_zone_id
+  name       = var.domain
+  value      = "protonmail-verification=87a1d2b07ad3698186abeb79feeceb9f19833a81"
+  type       = "TXT"
+  ttl        = 3600
+}
+
 resource "cloudflare_record" "root_record_old" {
   zone_id    = var.cloudflare_zone_id_old
   name       = var.domain_old
