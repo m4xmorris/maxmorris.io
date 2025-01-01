9cc88c8cae93e49c42d72800ce32b1c60cae124a
Maxwell Morris <max@morrislan.net>
2025-01-01 00:53:14 +0000

    fix

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -25,7 +25,7 @@ resource "cloudflare_record" "dkim" {
   }
   zone_id = var.cloudflare_zone_id
   name    = "${each.key}.maxmorris.io"
-  value   = each.value
+  content = each.value
   type    = "CNAME"
 }
 
