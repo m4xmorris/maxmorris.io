6739fd29703a159f47ab2c71041620ac4ee0192b
Maxwell Morris <max@morrislan.net>
2025-01-11 02:12:03 +0000

    things

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "morrislan-tfstate"
-    key    = "maxmorris-io/terraform.tfstate"
-    region = "us-east-1"
-    endpoints = { s3 = "https://lon1.digitaloceanspaces.com" }
+    bucket                      = "morrislan-tfstate"
+    key                         = "maxmorris-io/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoints                   = { s3 = "https://lon1.digitaloceanspaces.com" }
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
