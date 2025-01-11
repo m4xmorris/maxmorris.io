7b85310175e52e68cfcbfd71436847a216334089
Maxwell Morris <max@morrislan.net>
2025-01-11 02:10:45 +0000

    fixes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,17 +1,13 @@
 terraform {
   backend "s3" {
     bucket = "morrislan-tfstate"
-    key    = "/morrislan/terraform.tfstate"
+    key    = "maxmorris-io/terraform.tfstate"
     region = "us-east-1"
-    endpoint = "https://lon1.digitaloceanspaces.com"
+    endpoints = { s3 = "https://lon1.digitaloceanspaces.com" }
     skip_credentials_validation = true
-    skip_metadata_api_check = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
   }
-}
-
-resource "digitalocean_spaces_bucket" "tfstate" {
-  name   = "morrislan-tfstate"
-  region = "lon1"
-  versioning {enabled = "true"}
-  acl = "private"
 }
